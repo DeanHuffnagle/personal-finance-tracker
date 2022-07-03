6cbcd975b5c5441751b0b530806a10c8f9a22f9b
deanhuffnagle <huffnagleDean@gmail.com>
2022-07-02 22:54:11 -0400

    basic set up of spring security

--- src/main/java/com/example/personalFinanceTracker/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/personalFinanceTracker/security/ApplicationSecurityConfig.java
@@ -1,13 +1,48 @@
 package com.example.personalFinanceTracker.security;
 
 
+import com.example.personalFinanceTracker.filter.CustomAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+@Override
+protected void configure(HttpSecurity http) throws Exception {
+  CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+  http
+      .csrf()
+        .disable()
+      .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      .and()
+      .addFilter(customAuthenticationFilter)
+        .authorizeRequests()
+        .anyRequest()
+        .permitAll();
+
+}
+
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+
 }
--- src/main/java/com/example/personalFinanceTracker/service/UserServiceImplementation.java
+++ src/main/java/com/example/personalFinanceTracker/service/UserServiceImplementation.java
@@ -6,6 +6,10 @@ import com.example.personalFinanceTracker.repo.AccountRepo;
 import com.example.personalFinanceTracker.repo.AppUserRepo;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -13,15 +17,21 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Service @AllArgsConstructor @Transactional @Slf4j
-public class UserServiceImplementation implements UserService{
+@Service
+@AllArgsConstructor
+@Transactional
+@Slf4j
+public class UserServiceImplementation implements UserService, UserDetailsService {
 
   private final AppUserRepo userRepo;
   private final AccountRepo accountRepo;
 
+  private final PasswordEncoder passwordEncoder;
+
 
   @Override
   public AppUser saveUser(AppUser user) {
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
     return userRepo.save(user);
   }
 
@@ -40,5 +50,10 @@ public class UserServiceImplementation implements UserService{
   @Override
   public List<AppUser> getUsers() {
     return userRepo.findAll();
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return null;
   }
 }
